68b046ea7404f8234298b5591cb095f874e0e839
Aleks942 <aleks.aw1978@gmail.com>
2026-06-04 17:30:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -341,6 +341,13 @@ def analyze(ticker):
             OI_HISTORY[symbol] = []
 
         OI_HISTORY[symbol].append(oi)
+
+        print(
+            "[OI_LEN]",
+            symbol,
+            len(OI_HISTORY[symbol])
+        )
+        
     
         if len(OI_HISTORY[symbol]) > 20:
             OI_HISTORY[symbol].pop(0)
