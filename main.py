49b05fa079be6c08ca5ed82bb788213cb4052feb
Aleks942 <aleks.aw1978@gmail.com>
2026-06-05 18:05:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import time
 import requests
 import random
 from datetime import datetime, UTC
+
 from money_flow_engine import analyze_new_money
+
 from liquidation_engine import (
     start_liquidation_streams,
     fetch_okx_liquidations,
     get_liquidation_summary
 )
-from stats_engine import register_signal
+
+from stats_engine import (
+    register_signal,
+    update_signal_result
+)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
