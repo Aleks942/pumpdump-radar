677c13f3e7378fc0286e1b5c2f0aad31da65684a
Aleks942 <aleks.aw1978@gmail.com>
2026-06-08 11:34:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -814,15 +814,6 @@ L: ${long_liq:,.0f} | S: ${short_liq:,.0f}
 
 print("🚀 PumpDump Radar V2 started")
 
-print("AAAAAAAAAAAA")
-
-test_binance()
-
-print("BBBBBBBBBBBB")
-
-test_bybit()
-
-print("CCCCCCCCCCCC")
 
 test_binance()
 test_bybit()
