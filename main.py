7ee937f18f5fd2b41eb9d385bcb43880e304f8b1
Aleks942 <aleks.aw1978@gmail.com>
2026-07-06 15:23:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ MAX_SYMBOLS = int(os.getenv("MAX_SYMBOLS", 1000))
 symbol_states = {}
 signal_24h_count = {}
 OI_HISTORY = {}
+signal_memory = {}
 OI_CHANGE_HISTORY = {}
 print("[BOOT] OI_HISTORY CREATED")
 rotation_index = 0
