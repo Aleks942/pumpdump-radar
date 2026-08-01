0e0439693570ab1358b52a2dd08e7733abed030a
Aleks942 <aleks.aw1978@gmail.com>
2026-08-01 10:27:13 +0200

    Update chief_trader_v7.py

--- chief_trader_v7.py
+++ chief_trader_v7.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from typing import Any
 from adaptive_learning import load_weights
 
-ADAPTIVE_WEIGHTS = load_weights()
+# ADAPTIVE_WEIGHTS = load_weights()
 
 # ============================================
 # CHIEF TRADER V7
