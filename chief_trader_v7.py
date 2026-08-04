2c6c1e9c538277864ff39ed28000589d2e5c022c
Aleks942 <aleks.aw1978@gmail.com>
2026-08-04 23:21:01 +0200

    Update chief_trader_v7.py

--- chief_trader_v7.py
+++ chief_trader_v7.py
@@ -2349,6 +2349,14 @@ def build_market_context(signal, votes):
         2
     )
 
+    # ============================================
+    # STATE ENGINE
+    # ============================================
+    
+    context["market_state"] = detect_market_state(
+        context
+    )
+    
     return context
 
 # ============================================
