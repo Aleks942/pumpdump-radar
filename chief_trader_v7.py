ba410ab215f33a60dce769863ffc79bdcb5dbf71
Aleks942 <aleks.aw1978@gmail.com>
2026-08-29 21:26:38 +0200

    Update chief_trader_v7.py

--- chief_trader_v7.py
+++ chief_trader_v7.py
@@ -3757,6 +3757,12 @@ RULEBOOK = {
             "conditions": [
 
                 {
+                    "field": "allow_entry",
+                    "op": "==",
+                    "value": True,
+                },
+
+                {
                     "field": "entry_score",
                     "op": ">=",
                     "value": 80,
