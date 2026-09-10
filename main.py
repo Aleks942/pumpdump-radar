6232f02cd60be5670cdc3101c35ef1a13e62daba
Aleks942 <aleks.aw1978@gmail.com>
2026-09-10 13:19:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2651,6 +2651,15 @@ while True:
     signals = 0
     no_signal = 0
 
+    for tracked_symbol in list(ENTRY_TRACKER.keys()):
+        tracked_price = current_prices.get(tracked_symbol)
+
+        if tracked_price:
+            update_entry_tracker(
+                tracked_symbol,
+                tracked_price
+            )
+
     for ticker in current_chunk:
         checked += 1
 
