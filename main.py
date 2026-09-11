38b6178d80a8817befc6c5d6ed184cf7a2638694
Aleks942 <aleks.aw1978@gmail.com>
2026-09-12 00:23:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ import random
 import threading
 import requests
 from datetime import datetime, UTC
-
 from money_flow_engine import analyze_new_money
 from spot_cvd_engine import get_spot_cvd
-
-from smart_score_engine import calculate_smart_score
 from chief_trader_v7 import chief_trader_v7
 from telegram_builder import build_short_message
 from trade_flow_collector_v3 import get_futures_windows
