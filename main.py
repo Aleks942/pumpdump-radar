b3505c166936c6fb6519afc68dd56e587e1be4dd
Aleks942 <aleks.aw1978@gmail.com>
2026-09-13 21:53:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -68,29 +68,29 @@ TIME_WINDOWS = {
     "5m": {
         "bar": "1m",
         "candles": 5,
-        "pump": float(os.getenv("PUMP_THRESHOLD_5M", 5)),
-        "dump": -float(os.getenv("DUMP_THRESHOLD_5M", 5)),
+        "pump": float(os.getenv("PUMP_THRESHOLD_5M", 1.5))
+        "dump": -float(os.getenv("DUMP_THRESHOLD_5M", 1.5))
     },
 
     "10m": {
         "bar": "1m",
         "candles": 10,
-        "pump": float(os.getenv("PUMP_THRESHOLD_10M", 6)),
-        "dump": -float(os.getenv("DUMP_THRESHOLD_10M", 6)),
+        "pump": float(os.getenv("PUMP_THRESHOLD_10M", 2.0)),
+        "dump": -float(os.getenv("DUMP_THRESHOLD_10M", 2.0)),
     },
 
     "20m": {
         "bar": "1m",
         "candles": 20,
-        "pump": float(os.getenv("PUMP_THRESHOLD_20M", 7)),
-        "dump": -float(os.getenv("DUMP_THRESHOLD_20M", 7)),
+        "pump": float(os.getenv("PUMP_THRESHOLD_20M", 2.5)),
+        "dump": -float(os.getenv("DUMP_THRESHOLD_20M", 2.5)),
     },
 
     "30m": {
         "bar": "1m",
         "candles": 30,
-        "pump": float(os.getenv("PUMP_THRESHOLD_30M", 8)),
-        "dump": -float(os.getenv("DUMP_THRESHOLD_30M", 8)),
+        "pump": float(os.getenv("PUMP_THRESHOLD_30M", 3)),
+        "dump": -float(os.getenv("DUMP_THRESHOLD_30M", 3)),
     }
 
 }
