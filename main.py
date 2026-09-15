e24f774ac767d305f873baf23ab0edead0ae87a8
Aleks942 <aleks.aw1978@gmail.com>
2026-09-15 15:53:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ from market_memory import (
     update_market_memory,
     save_oi_snapshot,
     load_recent_oi_history,
-    get_similar_reversal_statistics,
+   
 )
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
